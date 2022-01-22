c4442fc152568f8a3bacd0fa5bf0c2b0827dd5cb
Zakir Baytar <zakirrbaytar@gmail.com>
2022-01-22 23:27:16 +0300

    Add error handling to bundler

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -11,16 +11,26 @@ export const bundle = async (rawCode: string) => {
     });
   }
 
-  const result = await service.build({
-    entryPoints: ["index.js"],
-    bundle: true,
-    write: false,
-    plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
-    define: {
-      "process.env.NODE_ENV": '"production"',
-      global: "window",
-    },
-  });
+  try {
+    const result = await service.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
 
-  return result.outputFiles[0].text;
+    return {
+      code: result.outputFiles[0].text,
+      error: null,
+    };
+  } catch (error: any) {
+    return {
+      code: "",
+      error: error.message,
+    };
+  }
 };
