2645b77f9c859c3303fcd1f9de18e0bcfb74962f
Zakir Baytar <zakirrbaytar@gmail.com>
2022-01-25 11:07:35 +0300

    Add interfaces for actions

---
+++ src/state/actions/index.ts
@@ -0,0 +1,41 @@
+import { ActionType } from "../action-types";
+import { CellType } from "../cell";
+
+export type Direction = "up" | "down";
+
+export interface MoveCellAction {
+  type: ActionType.MoveCell;
+  payload: {
+    id: string;
+    direction: Direction;
+  };
+}
+
+export interface InsertCellBeforeAction {
+  type: ActionType.InsertCellBefore;
+  payload: { id: string; type: CellType };
+}
+
+export interface InsertCellAfterAction {
+  type: ActionType.InsertCellAfter;
+  payload: { id: string; type: CellType };
+}
+
+export interface UpdateCellAction {
+  type: ActionType.UpdateCell;
+  payload: { id: string; content: string };
+}
+
+export interface DeleteCellAction {
+  type: ActionType.DeleteCell;
+  payload: {
+    id: string;
+  };
+}
+
+export type Action =
+  | MoveCellAction
+  | InsertCellBeforeAction
+  | InsertCellAfterAction
+  | UpdateCellAction
+  | DeleteCellAction;
