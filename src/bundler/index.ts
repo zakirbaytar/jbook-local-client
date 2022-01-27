a4dfef9f4425e1ca104de3f9931dc4b3d7d976b9
Zakir Baytar <zakirrbaytar@gmail.com>
2022-01-27 03:09:49 +0300

    Update bundler options to avoid name conflict

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -21,6 +21,8 @@ export const bundle = async (rawCode: string) => {
         "process.env.NODE_ENV": '"production"',
         global: "window",
       },
+      jsxFactory: "_React.createElement",
+      jsxFragment: "_React.Fragment",
     });
 
     return {
