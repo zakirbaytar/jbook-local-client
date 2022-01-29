4dc1366836a821100e7a88e3314c035224ce6974
Zakir Baytar <zakirrbaytar@gmail.com>
2022-01-29 21:08:44 +0300

    Add action creator for saving cells

--- src/state/action-creators/cells.ts
+++ src/state/action-creators/cells.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+import { RootState } from "..";
 import { ActionType } from "../action-types";
 import {
   MoveCellAction,
@@ -60,6 +61,27 @@ export const fetchCells = () => {
       if (axios.isAxiosError(error)) {
         dispatch({
           type: ActionType.FetchCellsError,
+          payload: { error: error.message },
+        });
+      }
+    }
+  };
+};
+
+export const saveCells = () => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
+    try {
+      const {
+        cells: { data, order },
+      } = getState();
+
+      const cells = order.map((id) => data[id]);
+
+      await axios.post("/cells", { cells });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        dispatch({
+          type: ActionType.SaveCellsError,
           payload: { error: error.message },
         });
       }
