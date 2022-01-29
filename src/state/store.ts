25617a306d0ef396fcddb3dc67cf585211df0165
Zakir Baytar <zakirrbaytar@gmail.com>
2022-01-29 21:09:19 +0300

    Add middleware to redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
+import { persistanceMiddleware } from "./middlewares/persistance-middleware";
+
 import reducers from "./reducers";
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(thunk, persistanceMiddleware)
+);
